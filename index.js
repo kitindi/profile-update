cba1e2da314870d9edc27ee843513e4b1f2bcf35
Abdulaziz Sadi <abdullkitindi@gmail.com>
2022-11-12 06:52:56 +0300

    added the banner and about section

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let barIcon = document.querySelector('#bars');
 let closeIcon = document.querySelector('#close');
 let navBar = document.querySelector('.nav-bar');
+let navLinks = document.querySelectorAll('.nav-link')
 
 barIcon.addEventListener('click',()=>{
   navBar.style.left ="0px"
@@ -12,4 +13,12 @@ closeIcon.addEventListener('click',()=>{
   navBar.style.left ="-100%"
   closeIcon.style.display = "none";
   barIcon.style.display = "block";
+})
+
+navLinks.forEach((link)=>{
+  link.addEventListener('click',()=>{
+    navBar.style.left ="-100%"
+  closeIcon.style.display = "none";
+  barIcon.style.display = "block";
+  })
 })
