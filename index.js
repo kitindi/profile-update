7614692cfd790d5a0deca77da460973a961a5c6d
Abdulaziz Sadi <abdullkitindi@gmail.com>
2022-11-27 17:55:25 +0300

    added the contact and projects section

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-let barIcon = document.querySelector('#bars');
-let closeIcon = document.querySelector('#close');
-let navBar = document.querySelector('.nav-bar');
-let navLinks = document.querySelectorAll('.nav-link')
+let barIcon = document.querySelector("#bars");
+let closeIcon = document.querySelector("#close");
+let navBar = document.querySelector(".nav-bar");
+let navLinks = document.querySelectorAll(".nav-link");
 
-barIcon.addEventListener('click',()=>{
-  navBar.style.left ="0px"
+barIcon.addEventListener("click", () => {
+  navBar.style.left = "0px";
   closeIcon.style.display = "block";
   barIcon.style.display = "none";
-})
+});
 
-closeIcon.addEventListener('click',()=>{
-  navBar.style.left ="-100%"
+closeIcon.addEventListener("click", () => {
+  navBar.style.left = "-100%";
   closeIcon.style.display = "none";
   barIcon.style.display = "block";
-})
+});
 
-navLinks.forEach((link)=>{
-  link.addEventListener('click',()=>{
-    navBar.style.left ="-100%"
-  closeIcon.style.display = "none";
-  barIcon.style.display = "block";
-  })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navBar.style.left = "-100%";
+    closeIcon.style.display = "none";
+    barIcon.style.display = "block";
+  });
+});
+
+// SLIDER
